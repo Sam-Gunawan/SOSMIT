a961d55ff03c6c6ad82c0720f29afc473aba9580
Samuel <samistheguns@gmail.com>
2025-06-25 16:02:15 +0700

    added asset condition and change reason columns

--- backend/internal/seed/init.sql
+++ backend/internal/seed/init.sql
@@ -64,12 +64,24 @@ CREATE TABLE "Asset" (
     "asset_tag" VARCHAR(12) PRIMARY KEY,
     "serial_number" VARCHAR(25) UNIQUE NOT NULL,
     "status" VARCHAR(20) NOT NULL CHECK ("status" IN ('Deployed', 'In Inventory', 'In Repair', 'Disposed', 'Down', 'On Loan')),
+    "status_reason" VARCHAR(20)
+    CHECK (
+        ("status" = 'Disposed' AND "status_reason" IS NOT NULL AND "status_reason" IN ('Lost', 'Obsolete'))
+        OR
+        ("status" <> 'Disposed' AND "status_reason" IS NULL)
+    ),
     "product_category" VARCHAR(50) NOT NULL CHECK ("product_category" IN ('Hardware', 'Software')),
     "product_subcategory" VARCHAR(50) NOT NULL CHECK ("product_subcategory" IN ('Processing Unit', 'Peripheral', 'Power Supply')),
     "product_variety" VARCHAR(50) NOT NULL CHECK ("product_variety" IN ('Laptop', 'Desktop', 'Monitor', 'Uninterrupted Power Supply', 'Personal Digital Assistant', 'Printer/Multifunction')),
     "brand_name" VARCHAR(25) NOT NULL,
     "product_name" VARCHAR(50) NOT NULL,
-
+    "condition" BOOLEAN NOT NULL DEFAULT TRUE, -- TRUE means the asset is in good condition, FALSE means it is not.
+    "condition_photo_url" TEXT
+    CHECK (
+        ("condition_photo_url" IS NULL AND "condition" = TRUE) -- If condition is good, no photo is required
+        OR
+        ("condition_photo_url" IS NOT NULL AND "condition" = FALSE) -- If condition is not good, a photo is required
+    ),
     -- Foreign key to User (owner of the asset). On delete set null means if the User is deleted, the owner_id in Asset will be set to NULL.
     "owner_id" INT NOT NULL REFERENCES "User"("user_id") ON DELETE SET NULL,
 
@@ -101,7 +113,12 @@ CREATE TABLE "OpnameSession" (
 CREATE TABLE "AssetChanges" (
     "id" SERIAL PRIMARY KEY,
     "changes" JSONB, -- Storing changes as a JSON object
+    "change_reason" TEXT NOT NULL, -- Reason for the change. It's required to provide context for the change.
+
+    -- Foreign key to OpnameSession (the opname session in which the changes were made).
     "session_id" INT NOT NULL REFERENCES "OpnameSession"("id"),
+    
+    -- Foreign key to Asset (the asset that was changed).
     "asset_tag" VARCHAR(12) NOT NULL REFERENCES "Asset"("asset_tag") ON DELETE CASCADE
 );
 
