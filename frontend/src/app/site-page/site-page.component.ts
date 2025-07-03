99ca358b4d13b2bd0b62014ea016ef5b7df0708c
priskaaimee <likarsaaimee@gmail.com>
2025-07-03 10:34:42 +0700

    add assets

--- frontend/src/app/site-page/site-page.component.ts
+++ frontend/src/app/site-page/site-page.component.ts
@@ -4,6 +4,7 @@ import { Siteinfo } from '../siteinfo';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { ActivatedRoute } from '@angular/router';
 import { Assetinfo } from '../assetinfo';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-site-page',
@@ -14,8 +15,8 @@ import { Assetinfo } from '../assetinfo';
 export class SitePageComponent {
   sitePage? : Siteinfo;
 
-  constructor(private route: ActivatedRoute) {
-    const siteList: Siteinfo[] = new DashboardComponent().siteCardList;
+  constructor(private route: ActivatedRoute, private apiService: ApiService) {
+    const siteList: Siteinfo[] = new DashboardComponent(this.apiService).siteCardList;
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.sitePage = siteList.find (site => site.siteID === id);
   }
@@ -23,17 +24,87 @@ export class SitePageComponent {
   assetCardList: Assetinfo[] = [
     {
       assetID: 1,
-      assetIcon: 
-      assetTag: SMPC00000001,
-      serialNumber: SGH7J2L9Q4M,
-      assetStatus: Deployed,
-      category: Hardware,
-      subCategory: Processing Unit,
-      productVariety: Desktop,
-      assetBrand: HP,
-      assetName: HP ProDesk 400 G4 SFF,
-      assetOwner: LESTARIL,
-      siteName: Head Office Jakarta,
+      assetIcon: '/assets/desktop.svg',
+      assetTag: 'SMPC00000001',
+      serialNumber: 'SGH7J2L9Q4M',
+      assetStatus: 'Deployed',
+      category: 'Hardware',
+      subCategory: 'Processing Unit',
+      productVariety: 'Desktop',
+      assetBrand: 'HP',
+      assetName: 'HP ProDesk 400 G4 SFF',
+      assetOwner: 'LESTARIL',
+      siteName: 'Head Office Jakarta'
+    },
+    {
+      assetID: 2,
+      assetIcon: '/assets/laptop.svg',
+      assetTag: 'SMLT00000003',
+      serialNumber: '5CD2597SON',
+      assetStatus: 'Deployed',
+      category: 'Hardware',
+      subCategory: 'Processing Unit',
+      productVariety: 'Laptop',
+      assetBrand: 'HP',
+      assetName: 'HP Notebook',
+      assetOwner: 'PUJIA',
+      siteName: 'Area Marketing Office Denpasar'
+    },
+    {
+      assetID: 3,
+      assetIcon: '/assets/monitor.svg',
+      assetTag: 'SMMN00000001',
+      serialNumber: '3CQ9175CNT',
+      assetStatus: 'Deployed',
+      category: 'Hardware',
+      subCategory: 'Peripheral',
+      productVariety: 'Monitor',
+      assetBrand: 'HP',
+      assetName: 'HP LE1901w 19-inch Widescreen LCD',
+      assetOwner: 'LESTARIL',
+      siteName: 'Head Office Jakarta'
+    },
+    {
+      assetID: 4,
+      assetIcon: '/assets/ups.svg',
+      assetTag: 'SMPU00000001',
+      serialNumber: '3B3455X20483',
+      assetStatus: 'Deployed',
+      category: 'Hardware',
+      subCategory: 'Power Supply',
+      productVariety: 'Uninterrupted Power Supply',
+      assetBrand: 'APC',
+      assetName: 'APC Back-UPS Pro 550 VA',
+      assetOwner: 'LESTARIL',
+      siteName: 'Head Office Jakarta'
+    },
+    {
+      assetID: 5,
+      assetIcon: '/assets/handheld.svg',
+      assetTag: 'SMTP00000001',
+      serialNumber: 'S20254578225064',
+      assetStatus: 'Deployed',
+      category: 'Hardware',
+      subCategory: 'Processing Unit',
+      productVariety: 'Personal Digital Assistant',
+      assetBrand: 'Zebra',
+      assetName: 'Zebra TC26',
+      assetOwner: 'EKKOF',
+      siteName: 'Area Marketing Office Denpasar'
+    },
+    {
+      assetID: 6,
+      assetIcon: '/assets/printer.svg',
+      assetTag: 'SMPR00000003',
+      serialNumber: 'XXZHJ231947513',
+      assetStatus: 'Deployed',
+      category: 'Hardware',
+      subCategory: 'Peripheral',
+      productVariety: 'Printer/Multifunction',
+      assetBrand: 'Zebra',
+      assetName: 'Thermal Printer Zebra ZQ320 Plus',
+      assetOwner: 'ANANDAA',
+      siteName: 'Area Marketing Office Denpasar'
     }
   ]
 }
