28ea68aee2ec1db544e9f180147256ed498ae2c7
priskaaimee <likarsaaimee@gmail.com>
2025-07-04 11:52:21 +0700

    site page v.1

---
+++ frontend/src/app/asset-page/asset-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asset-page',
+  imports: [],
+  templateUrl: './asset-page.component.html',
+  styleUrl: './asset-page.component.scss'
+})
+export class AssetPageComponent {
+
+}
