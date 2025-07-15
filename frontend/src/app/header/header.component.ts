3f67b59320716487c22b38ba7911e4ddecc9ccb6
priskaaimee <likarsaaimee@gmail.com>
2025-07-15 14:25:45 +0700

    add profile modal for reminder and log out button

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OpnameSession } from '../model/opname-session.model';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +10,39 @@ import { OpnameSession } from '../model/opname-session.model';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  currentView: 'large' | 'small' = 'large';
+  isMobile: boolean = false;
   notif!: OpnameSession;
 
   notifications = [
     { message: 'New Opname session created...', isRead: false},
     { message: 'Opname session completed...', isRead: true},
   ];
+
+  constructor(private apiService: ApiService) {
+  }
+
+  ngOnInit(): void {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize() {
+    this.isMobile = window.innerWidth < 610; // Define mobile breakpoint
+    
+    if (this.isMobile) {
+      this.currentView = 'small'; // Force list view on mobile
+    } else {
+      this.currentView = 'large'; // Default to card view on desktop
+    }
+  }
+
+  logout(): void {
+    // Call the logout method from ApiService
+    this.apiService.logout();
+  }
 }
