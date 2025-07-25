9e7df6555c7165c3fc85430dadf773b5e2999b62
Samuel <samistheguns@gmail.com>
2025-07-25 09:43:25 +0700

    added user name info at settings modal and changed a user's info

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OpnameSession } from '../model/opname-session.model';
 import { ApiService } from '../services/api.service';
+import { User } from '../model/user.model';
 
 @Component({
   selector: 'app-header',
@@ -14,22 +15,34 @@ export class HeaderComponent {
   isMobile: boolean = false;
   notif!: OpnameSession;
   showLogoutConfirmation = false;
+  loggedInUser: User & { fullName: string } = {} as User & { fullName: '' }; // User object with full name
 
   notifications = [
     { message: 'New Opname session created...', isRead: false},
     { message: 'Opname session completed...', isRead: true},
   ];
 
-  constructor(private apiService: ApiService) {
-  }
+  constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.checkScreenSize();
+    this.fetchLoggedInUserProfile();
   }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
     this.checkScreenSize();
+  }
+
+  private fetchLoggedInUserProfile(): void {
+    this.apiService.getUserProfile('me').subscribe(
+      (user: User) => {
+        this.loggedInUser = { ...user, fullName: `${user.firstName} ${user.lastName}` };
+      },
+      (error) => {
+        console.error('Error fetching user profile:', error);
+      }
+    );
   }
 
   private checkScreenSize() {
