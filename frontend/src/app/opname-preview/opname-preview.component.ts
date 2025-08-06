fba08611b0be3c7437cefff0117801469253079f
priskaaimee <likarsaaimee@gmail.com>
2025-08-06 09:31:39 +0700

    update styles and structure for opname components

--- frontend/src/app/opname-preview/opname-preview.component.ts
+++ frontend/src/app/opname-preview/opname-preview.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-opname-preview',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './opname-preview.component.html',
   styleUrl: './opname-preview.component.scss'
 })
 export class OpnamePreviewComponent {
-
+  // errorMessage: string = '';
+  // showToast: boolean = false;
+  // isLoading: boolean = true;
 }
